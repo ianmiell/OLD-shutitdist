826a5e5ef06b6b960ecf9d7be462fc2668cb7f23
Ian Miell <ian.miell@gmail.com>
2014-12-05 18:18:00 +0000

    new modules leading up to java

---
+++ pango/pango.py
@@ -0,0 +1,50 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class pango(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('mkdir -p /tmp/build/pango')
+		shutit.send('cd /tmp/build/pango')
+		shutit.send('curl -L http://ftp.gnome.org/pub/gnome/sources/pango/1.36/pango-1.36.7.tar.xz | xz -d | tar -xf -')
+		shutit.send('cd pango*')
+		shutit.send('./configure --prefix=/usr --sysconfdir=/etc')
+		return True
+
+	#def get_config(self, shutit):
+	#	shutit.get_config(self.module_id,'item','default')
+	#	return True
+
+	#def check_ready(self, shutit):
+	#	return True
+	
+	#def start(self, shutit):
+	#	return True
+
+	#def stop(self, shutit):
+	#	return True
+
+	#def finalize(self, shutit):
+	#	return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return pango(
+		'shutit.tk.sd.pango.pango', 158844782.0119,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.sd.cairo.cairo','shutit.tk.sd.harfbuzz.harfbuzz','shutit.tk.sd.xorg_libs.xorg_libs']
+	)
+
---
+++ pixman/pixman.py
@@ -0,0 +1,52 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class pixman(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return shutit.file_exists('/root/shutit_build/module_record/' + self.module_id + '/built')
+
+	def build(self, shutit):
+		shutit.send('mkdir -p /tmp/build/pixman')
+		shutit.send('cd /tmp/build/pixman')
+		shutit.send('curl -L http://cairographics.org/releases/pixman-0.32.6.tar.gz | tar -zxf -')
+		shutit.send('cd pixman*')
+		shutit.send('./configure --prefix=/usr --disable-static')
+		shutit.send('make')
+		shutit.send('make install')
+		return True
+
+	#def get_config(self, shutit):
+	#	shutit.get_config(self.module_id,'item','default')
+	#	return True
+
+	#def check_ready(self, shutit):
+	#	return True
+	
+	#def start(self, shutit):
+	#	return True
+
+	#def stop(self, shutit):
+	#	return True
+
+	#def finalize(self, shutit):
+	#	return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return pixman(
+		'shutit.tk.sd.pixman.pixman', 158844782.0116,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.sd.setup.setup']
+	)
+
